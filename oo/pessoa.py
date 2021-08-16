d87259b58e6076c6e28587e8d9e53d33742e0180
eduspj <&e91667013>
2021-08-16 13:01:01 -0700

    Criada atributos de instâncias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,19 @@
-class Pessoa:
-    def cumprimentar(self):
+class Pessoa:  # Classe
+    def __init__(self, nome=None, idade=38):  # atributo de dados
+        self.nome = nome
+        self.idade = idade
+
+
+    def cumprimentar(self): #
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Júnior')  # cria o objeto
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Eduardo'
+    print(p.nome)
+    print(p.idade)
